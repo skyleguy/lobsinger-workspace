42774c26bcb0f59a88d23c8345557537a083213b
Skyleguy <kylelobsinger@gmail.com>
2022-12-21 17:00:57 -0500

    spruce up prettierrc

--- apps/glist/src/app/app.component.ts
+++ apps/glist/src/app/app.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'glist-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  constructor() {
-    console.log('hello there');
-  }
-}
+export class AppComponent {}
