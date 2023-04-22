dcaecee52e24055914f842ceaf86f81c5745c57e
Skyleguy <kylelobsinger@gmail.com>
2023-04-22 09:49:13 -0400

    replace shiba image with image icon. had to put in assets to use as img tag within materical card which was annoying

--- apps/glist/src/app/app.component.ts
+++ apps/glist/src/app/app.component.ts
@@ -5,8 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  constructor() {
-    console.log('hello!');
-  }
-}
+export class AppComponent {}
