852398df5924ee22705afe3fae15f26e765d0abe
Kyle <kylelobsinger@gmail.com>
2024-11-27 08:19:33 -0500

    attempted to add project specific firebase config and change config b… (#28)
    Co-authored-by: Kyle Lobsinger <ky@kyles-mbp.lan>

--- apps/aat/src/app/app.component.ts
+++ apps/aat/src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { getAnalytics } from 'firebase/analytics';
 import { initializeApp } from 'firebase/app';
 
 import { AppContainerComponent } from '@lob/client/shared/layout/ui';
+
+import { environment } from '../environments/environment';
 
 @Component({
   standalone: true,
@@ -26,17 +29,7 @@ import { AppContainerComponent } from '@lob/client/shared/layout/ui';
 export class AppComponent {
   title = 'aat';
 
-  private firebaseConfig = {
-    apiKey: 'AIzaSyBIRW-yEJo94-F2HB1Assiz9oKCkTKZ1oQ',
-    authDomain: 'qr-reader-352914.firebaseapp.com',
-    projectId: 'qr-reader-352914',
-    storageBucket: 'qr-reader-352914.firebasestorage.app',
-    messagingSenderId: '1043209938754',
-    appId: '1:1043209938754:web:cc40a950203f9fc2fa4adb',
-    measurementId: 'G-0NJLDHTZQV'
-  };
-
   // Initialize Firebase
-  private app = initializeApp(this.firebaseConfig);
-  // private analytics = getAnalytics(app);
+  private app = initializeApp(environment.firebase);
+  private analytics = getAnalytics(this.app);
 }
---
+++ apps/aat/src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyDMJjRVoHmRfuySBFNzlFvfDHHVBbfYeoE',
+    authDomain: 'lobsinger-workspace-dev-e45e7.firebaseapp.com',
+    projectId: 'lobsinger-workspace-dev-e45e7',
+    storageBucket: 'lobsinger-workspace-dev-e45e7.firebasestorage.app',
+    messagingSenderId: '151978710816',
+    appId: '1:151978710816:web:c253e76021ec4095eca945',
+    measurementId: 'G-3DNPJS7ZE4'
+  }
+};
