6bba6d0179f0a27532a7ff6caff11371f792a153
Kyle Lobsinger <ky@Kyles-MacBook-Pro.local>
2024-12-23 16:41:26 -0500

    add env for production

--- apps/aat/src/environments/environment.ts
+++ apps/aat/src/environments/environment.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  firebase: {}
+  firebase: {
+    apiKey: 'AIzaSyDL9WxF63ZE6pxDn1bxiItR3OWDzkF620g',
+    authDomain: 'lobsinger-workspace-dev-e45e7.firebaseapp.com',
+    projectId: 'lobsinger-workspace-dev-e45e7',
+    storageBucket: 'lobsinger-workspace-dev-e45e7.firebasestorage.app',
+    messagingSenderId: '151978710816',
+    appId: '1:151978710816:web:c253e76021ec4095eca945',
+    measurementId: 'G-3DNPJS7ZE4'
+  }
 };
