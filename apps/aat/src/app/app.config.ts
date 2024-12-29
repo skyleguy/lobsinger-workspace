d37e5a5dcb091a12897e7cbb73b599b2f38972de
Kyle Lobsinger <ky@Kyles-MBP.home>
2024-12-29 18:52:53 -0500

    add pwa to aat

--- apps/aat/src/app/app.config.ts
+++ apps/aat/src/app/app.config.ts
@@ -1,8 +1,9 @@
 import { provideHttpClient } from '@angular/common/http';
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
+import { provideServiceWorker } from '@angular/service-worker';
 
 import { FirebaseOptionsToken } from '@lob/client/shared/firebase/data';
 
@@ -20,6 +21,10 @@ export const appConfig: ApplicationConfig = {
     {
       provide: MAT_SNACK_BAR_DEFAULT_OPTIONS,
       useValue: { duration: 3000 }
-    }
+    },
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ]
 };
