b0bea63ae533fc2fe5a917c7ffe4bed2296093b5
Kyle Lobsinger <ky@Kyles-MacBook-Pro.local>
2025-05-13 16:38:16 -0400

    changes to get theme service to auto update theme based on if the users preferred color scheme changes

--- apps/home/src/app/app.component.ts
+++ apps/home/src/app/app.component.ts
@@ -1,20 +1,18 @@
 import { Component } from '@angular/core';
-import { ButtonModule } from 'primeng/button';
-import { Ripple } from 'primeng/ripple';
 
 import { AppContainerComponent } from '@lob/client/shared/layout/ui';
 import { ThemeTogglerComponent } from '@lob/client/shared/theme/ui';
 
 @Component({
   selector: 'home-root',
-  imports: [AppContainerComponent, ThemeTogglerComponent, ButtonModule, Ripple],
+  imports: [AppContainerComponent, ThemeTogglerComponent],
   template: `
     <shared-layout-ui-app-container [isMaterialTheming]="false">
       <ng-container nav>
-        <h2>Smart Home</h2>
-        <button pButton pRipple>
-          <span pButtonLabel>label</span>
-        </button>
+        <div class="flex items-center gap-3">
+          <i class="fa-solid fa-home"></i>
+          <h2>Smart Home</h2>
+        </div>
         <shared-theme-ui-theme-toggler></shared-theme-ui-theme-toggler>
       </ng-container>
       <ng-container sidebar> sidebar </ng-container>
--- libs/client/shared/theme/ui/src/lib/services/theme.service.ts
+++ libs/client/shared/theme/ui/src/lib/services/theme.service.ts
@@ -1,6 +1,8 @@
 import { MediaMatcher } from '@angular/cdk/layout';
 import { effect, inject, linkedSignal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { signalStore, withHooks, withProps, withState } from '@ngrx/signals';
+import { fromEvent, map } from 'rxjs';
 
 import { PlatformService } from '@lob/client/shared/platform/ui';
 import { darkThemeClass } from '@lob/client/shared/theme/data';
@@ -8,30 +10,51 @@ import { darkThemeClass } from '@lob/client/shared/theme/data';
 const themeStorageKey = 'lobsinger-suite-theme';
 
 interface ThemeServiceState {
-  _isDarkMode: boolean;
+  _themeFromSystem: 'light' | 'dark';
   _persistedTheme: 'light' | 'dark' | null;
+  _mediaMatcher: MediaMatcher;
+  _platformService: PlatformService;
+}
+
+function getThemeFromSystem(mediaMatcher: MediaMatcher): 'light' | 'dark' {
+  return mediaMatcher.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
 }
 
 export const ThemeService = signalStore(
   { providedIn: 'root' },
-  withState<ThemeServiceState>((platformService = inject(PlatformService), mediaMatcher = inject(MediaMatcher)) => ({
-    _isDarkMode: mediaMatcher.matchMedia('(prefers-color-scheme: dark)').matches,
-    _persistedTheme: platformService.isBrowser ? (localStorage.getItem(themeStorageKey) as 'light' | 'dark' | null) : null
+  withState<ThemeServiceState>((mediaMatcher = inject(MediaMatcher), platformService = inject(PlatformService)) => ({
+    _themeFromSystem: getThemeFromSystem(mediaMatcher),
+    _persistedTheme: platformService.isBrowser ? (localStorage.getItem(themeStorageKey) as 'light' | 'dark' | null) : null,
+    _mediaMatcher: mediaMatcher,
+    _platformService: platformService
+  })),
+  withProps((store) => ({
+    prefersColorSchemeChange: toSignal(
+      fromEvent(store._mediaMatcher().matchMedia('(prefers-color-scheme: dark)'), 'change').pipe(
+        map(() => getThemeFromSystem(store._mediaMatcher()))
+      )
+    )
   })),
   withProps((store) => ({
     currentTheme: linkedSignal(() => {
-      const isDarkMode = store._isDarkMode();
+      const themeFromSystem = store._themeFromSystem();
       const persistedTheme = store._persistedTheme();
       if (persistedTheme) {
         return persistedTheme;
       }
-      return isDarkMode ? 'dark' : 'light';
+      return themeFromSystem;
     })
   })),
   withHooks({
-    onInit(store, platformService = inject(PlatformService)) {
+    onInit(store) {
       effect(() => {
-        if (platformService.isBrowser) {
+        const prefersColorSchemeChange = store.prefersColorSchemeChange();
+        if (prefersColorSchemeChange) {
+          store.currentTheme.set(prefersColorSchemeChange);
+        }
+      });
+      effect(() => {
+        if (store._platformService().isBrowser) {
           const theme = store.currentTheme();
           localStorage.setItem(themeStorageKey, theme);
           const html = document.documentElement;
