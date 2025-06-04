8ea96b6be4b4a44c3caee20a1035ce711b6d6a93
Kyle Lobsinger <ky@Kyles-MacBook-Pro.local>
2025-06-04 15:44:42 -0400

    getting close with pdfmake but seems like it doesnt support editable fields

--- apps/rqbd/src/app/components/iframed-pdf-viewer.component.ts
+++ apps/rqbd/src/app/components/iframed-pdf-viewer.component.ts
@@ -1,4 +1,5 @@
 import { AfterViewInit, Component, effect, inject, linkedSignal, signal } from '@angular/core';
+import { format } from 'date-fns';
 import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 import * as pdfMake from 'pdfmake/build/pdfmake';
 import * as pdfFonts from 'pdfmake/build/vfs_fonts';
@@ -17,16 +18,7 @@ import { ThemeService } from '@lob/client/shared/theme/ui';
     class: 'h-full w-full'
   },
   template: ` @if (generatedFileBlob(); as generatedFileBlob) {
-    <!-- @for (imageSrc of imageSources(); track imageSrc) {
-      <img [src]="imageSrc" />
-    } -->
-    <ngx-extended-pdf-viewer
-      [src]="generatedFileBlob"
-      [handTool]="false"
-      [showToolbar]="false"
-      [textLayer]="false"
-      [theme]="themeService.currentTheme()"
-    >
+    <ngx-extended-pdf-viewer [src]="generatedFileBlob" [handTool]="false" [textLayer]="false" [theme]="themeService.currentTheme()">
     </ngx-extended-pdf-viewer>
   }`
 })
@@ -38,15 +30,77 @@ export class IframedPdfViewerComponent implements AfterViewInit {
   protected readonly imageSources = signal<string[]>([]);
   protected readonly docDefinition = linkedSignal<TDocumentDefinitions>(() => {
     let content: Content[] = [
-      'First paragraph',
-      'Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines'
+      {
+        columns: [
+          {
+            width: 'auto',
+            columns: [
+              {
+                width: 'auto',
+                text: [
+                  {
+                    text: 'RequestBuilder provided by:',
+                    bold: true
+                  },
+                  {
+                    text: 'Advantage Inspection Service',
+                    italics: true
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            width: '*',
+            text: "Buyer's Full Name",
+            alignment: 'right'
+          }
+        ]
+      },
+      {
+        text: 'Property Address',
+        alignment: 'right'
+      },
+      {
+        text: 'City, State, Zip',
+        alignment: 'right'
+      },
+      {
+        text: format(new Date(), 'MM/dd/yyyy'),
+        alignment: 'right'
+      },
+      {
+        text: 'Repair Request Attachment',
+        color: '#286cb4',
+        alignment: 'center',
+        fontSize: 20,
+        bold: true
+      },
+      {
+        text: 'Buyer requests the following to be evaluated and corrected by qualified professionals:',
+        alignment: 'center',
+        italics: true
+      }
     ];
     const imageSources = this.imageSources();
     if (imageSources) {
       content = content.concat(
-        imageSources.map((url) => ({
-          image: url
-        }))
+        imageSources.map((url, index) => [
+          {
+            text: `Buyer's Request #${index}:`,
+            bold: true
+          },
+          {
+            text: 'Type request here...'
+          },
+          {
+            image: url,
+            width: 500
+          },
+          {
+            text: `Add another selection to Buyer's Request #${index}`
+          }
+        ])
       );
     }
     return {
