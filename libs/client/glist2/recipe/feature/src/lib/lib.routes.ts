eec123f5b311bd15168119a32e830f704b2cfc67
Kyle Lobsinger <ky@mac.lan>
2025-08-03 12:46:17 -0400

    remove glist 2 and add zxing browser

--- libs/client/glist2/recipe/feature/src/lib/lib.routes.ts
+++
@@ -1,5 +0,0 @@
-import { Route } from '@angular/router';
-
-import { RecipeContainerComponent } from './recipe-container/recipe-container.component';
-
-export const recipeRoutes: Route[] = [{ path: '', component: RecipeContainerComponent }];
