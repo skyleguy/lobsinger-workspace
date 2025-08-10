978bafdc151d10057c454048787ea7adbd746755
Kyle Lobsinger <ky@mac.lan>
2025-08-10 13:07:04 -0400

    optimize space on mobile by switching to select from autocomplete and modifying theme to reduce card padding in combination with reducing flex gaps

--- apps/aat/src/app/app.config.ts
+++ apps/aat/src/app/app.config.ts
@@ -3,8 +3,6 @@ import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angul
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 import { provideServiceWorker } from '@angular/service-worker';
-import { definePreset } from '@primeng/themes';
-import Material from '@primeng/themes/material';
 import { MessageService } from 'primeng/api';
 import { providePrimeNG } from 'primeng/config';
 
@@ -12,27 +10,9 @@ import { FirebaseOptionsToken } from '@lob/client/shared/firebase/data';
 import { darkThemeSelector } from '@lob/client/shared/theme/data';
 
 import { appRoutes } from './app.routes';
+import { AAT } from './primeng-preset';
 
 import { environment } from '../environments/environment';
-
-const AAT = definePreset(Material, {
-  semantic: {
-    primary: {
-      50: '{green.50}',
-      100: '{green.100}',
-      200: '{green.200}',
-      300: '{green.300}',
-      400: '{green.400}',
-      500: '{green.500}',
-      600: '{green.600}',
-      700: '{green.700}',
-      800: '{green.800}',
-      900: '{green.900}',
-      950: '{green.950}'
-    }
-  },
-  components: {}
-});
 
 export const appConfig: ApplicationConfig = {
   providers: [
