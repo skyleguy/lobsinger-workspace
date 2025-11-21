789c328999b5d46059f629aabcbd5705a36a7316
Kyle Lobsinger <ky@kyles-mbp.lan>
2025-11-21 11:14:56 -0500

    firebase and google requests are now set up to be recorded and replayed through the use of har files. no more increased timeouts to handle if cloud functions are cold and no more issues with the 429 Too many Requests coming from the users profile image.

--- apps/aat/e2e/specs/critical-flow/critical-flow.spec.ts
+++ apps/aat/e2e/specs/critical-flow/critical-flow.spec.ts
@@ -1,6 +1,12 @@
 import { expect } from '@playwright/test';
 
-import { test } from './setup/auth.setup';
+import { interceptFirebaseRequests, interceptGoogleRequests } from '../../routes';
+import { test } from '../../setup/auth.setup';
+
+/**
+ * when needing to re-record har files just set this to true and all marked requests will be recorded
+ */
+const isRecordingTraffic = false;
 
 test('not signed in', async ({ page }) => {
   const projectName = test.info().project.name;
@@ -11,10 +17,14 @@ test('not signed in', async ({ page }) => {
   await expect(page).toHaveScreenshot(`not-signed-in-${projectName}.png`);
 });
 
-test('critical signed in flow', async ({ page, auth, context }) => {
+test('critical signed in flow', async ({ auth, context }) => {
+  interceptFirebaseRequests(context, isRecordingTraffic);
+  interceptGoogleRequests(context, isRecordingTraffic);
+
   const projectName = test.info().project.name;
   const fakeLocation = { latitude: 37.7749, longitude: -122.4194 };
   await context.setGeolocation(fakeLocation);
+  const page = await context.newPage();
   await page.goto('/');
   await auth.login(page);
   await page.reload();
@@ -27,9 +37,7 @@ test('critical signed in flow', async ({ page, auth, context }) => {
   await expect(page.locator('[data-test="asset-track-container"]')).toBeVisible();
   expect(page.url()).toContain('/radon/12');
   const addressInput = page.locator('[data-test="asset-address"]');
-  await expect(addressInput).toHaveValue(/San Francisco/, {
-    timeout: 10000
-  });
+  await expect(addressInput).toHaveValue(/San Francisco/);
   await expect(page).toHaveScreenshot(`set-up-disabled-${projectName}.png`);
 
   const roomInput = page.locator('[data-test="asset-room-location"]');
@@ -44,13 +52,11 @@ test('critical signed in flow', async ({ page, auth, context }) => {
   const assignAssetButton = page.locator('[data-test="assign-asset-button"]');
   await assignAssetButton.click();
   const loading = page.locator('p-progress-spinner');
-  await expect(loading).toBeVisible();
-  await expect(loading).toBeHidden({
-    timeout: 10000
-  });
+  await expect(loading).toBeHidden();
   await expect(page).toHaveScreenshot(`after-submit-${projectName}.png`);
 
   const backButton = page.locator('[data-test="back-button"]:visible');
   await backButton.click();
   expect(page.url()).toContain('/scan');
+  await context.close();
 });
